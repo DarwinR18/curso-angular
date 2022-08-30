b9f67a0ce856877521a420306d02b98e968758ea
DarwinR18 <64547511+DarwinR18@users.noreply.github.com>
2022-08-29 22:51:54 -0500

    darwinr18: SharedModule and fragmentation code

---
+++ gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  buscar(): void {
+   const valor = this.txtBuscar.nativeElement.value;
+   console.log(valor);
+
+   this.txtBuscar.nativeElement.value="";
+  }
+
+}
